2f3d3a7c07257768624608c29be1a4304fb388e8
Johnatan Ribeiro <johnatan.jcr@gmail.com>
2020-07-27 11:01:34 -0300

    primeiro commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+// import ButtonLink from './ButtonLink';
+import Button from '../Button'
+
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="PekenaFlix"/>
+            </a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+    )
+}
+
+
+export default Menu
